9008fc5218afb50c5441893534c31733061828a0
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-01-31 12:54:59 +0530

    changed push and pop

--- stack.c
+++ stack.c
@@ -1,5 +1,6 @@
 #include "stack.h"
 #include <stdio.h>
+
 Stack createStack(void) {
 	LinkedList*  list= calloc(sizeof(LinkedList),1);
 	Stack stack = {list,&list->tail};
@@ -10,11 +11,11 @@ int push(Stack stack, void* data) {
 	Node* element = create_node(data);
 	int task = add_to_list(stack.stack,element);
 	stack.top = stack.stack->head;
-	return stack.stack->count;
+	return (task) ? stack.stack->count : -1;
 }
 	
 void* pop(Stack stack) {
-	void* result = deleteElementAt(stack.stack,stack.stack->count);
+	void* result = deleteElementAt(stack.stack,stack.stack->count-1);
 	stack.top = stack.stack->tail;
 	return result;	
 }
